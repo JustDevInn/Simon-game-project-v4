bbb243abd4ea896ace7b16ac66a0482313c7187f
Justin Peeters <Peeters.justin@yahoo.com>
2020-03-05 11:26:33 +0100

    version3

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ var colorPattern = [];
 var clickedPattern = [];
 var started = false;
 var level = 0;
+var startButton = document.querySelector('.startBtn');
 
 
-document.querySelector("#level-title").addEventListener("click", someFunction => {
-    if (!started) {
-        document.querySelector("#level-title").text = ("level " + level);
-        numberSequence();
-        started = true;
-    }
-});
+// document.querySelector("#level-title").addEventListener("click", someFunction => {
+//     if (!started) {
+//         document.querySelector("#level-title").text = ("level " + level);
+//         numberSequence();
+//         started = true;
+//     }
+// });
 
-
-// USER INPUT >>CLICK>>LIGHTNEN UP>>PUSH IN USER ARRAY
 document.querySelector('.btn.yellow').addEventListener("click", function() {
     clickedPattern.push("yellow");
     document.querySelector('.btn.yellow').classList.add('active')
     setTimeout(function() {
         document.querySelector('.btn.yellow').classList.remove('active')
-    }, 100);
+    }, 150);
     checkAnswer(clickedPattern.length - 1);
 
 
@@ -31,7 +30,7 @@ document.querySelector('.btn.red').addEventListener("click", function() {
     document.querySelector('.btn.red').classList.add('active')
     setTimeout(function() {
         document.querySelector('.btn.red').classList.remove('active')
-    }, 100);
+    }, 150);
     checkAnswer(clickedPattern.length - 1);
 
 });
@@ -41,7 +40,7 @@ document.querySelector('.btn.blue').addEventListener("click", function() {
     document.querySelector('.btn.blue').classList.add('active')
     setTimeout(function() {
         document.querySelector('.btn.blue').classList.remove('active')
-    }, 100);
+    }, 150);
     checkAnswer(clickedPattern.length - 1);
 
 });
@@ -51,11 +50,12 @@ document.querySelector('.btn.green').addEventListener("click", function() {
     document.querySelector('.btn.green').classList.add('active')
     setTimeout(function() {
         document.querySelector('.btn.green').classList.remove('active')
-    }, 100);
+    }, 150);
     checkAnswer(clickedPattern.length - 1);
 
 });
 
+// CHECKING USER ANSWER RIGHT/WRONG
 function checkAnswer(currentLevel) {
     if (colorPattern[currentLevel] === clickedPattern[currentLevel]) {
         if (clickedPattern.length === colorPattern.length) {
@@ -64,27 +64,35 @@ function checkAnswer(currentLevel) {
             }, 1000);
         }
     } else {
-        document.querySelector("#level-title").innerHTML = "☠️ ->rEFreSH<- ☠️";
+        document.querySelector("#level-title").innerHTML = "☠️GAME OVER☠️";
         startOver();
     }
 }
-
+// GENERATES A NEW SEQUENCE.
 function numberSequence() {
     clickedPattern = [];
     level++;
     document.querySelector("#level-title").innerHTML = ("Level " + level);
+
     const randomNr = Math.floor(Math.random() * 4);
     var randomColor = buttonColors[randomNr];
     colorPattern.push(randomColor);
     document.querySelector(`.btn.${randomColor}`).classList.add('active');
     setTimeout(function() {
         document.querySelector(`.btn.${randomColor}`).classList.remove('active');
-    }, 100);
+    }, 150);
     checkAnswer(clickedPattern.length - 1);
 }
-numberSequence();
 
 
+startButton.addEventListener("click", function startGame() {
+
+    startButton.classList.add('active')
+    setTimeout(function() {
+        startButton.style.display = 'none';
+    }, 100);
+
+});
 
 
 
@@ -93,4 +101,5 @@ function startOver() {
     level = 0;
     colorPattern = [];
     started = false;
+    startGame();
 }
